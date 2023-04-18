ce6f522d84c7ca5e5f866a0e23294326538cb249
ECK <57802146+EckGP@users.noreply.github.com>
2023-04-18 14:57:15 -0400

    Update Eckbot.py

--- Eckbot.py
+++ Eckbot.py
@@ -12,4 +12,4 @@ async def on_member_join(member):
     if guild.system_channel is not None:
         await guild.system_channel.send(f'Welcome {member.mention}! Please visit the #roles channel to select your roles.')
 
-client.run('MTA5MDI1NzgzMzMzMDczNzI2NQ.G6UdTz.bi7y64Qw8dl8I7P3GOd6sZxsKGipf6I9l3qwi4')
+client.run(os.environ['DISCORD_TOKEN'])
